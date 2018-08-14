9aed7fb874d0403e4163641bccb14e5802863251
shyaman_madhawa <shayaman321@gmail.com>
2018-08-14 20:56:07 +0530

    remove unwanted

--- _site/static/js/scroll.js
+++ _site/static/js/scroll.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
 });
 
 var cbpAnimatedHeader = (function() {
-    var changeHeaderOn = 200, scrollDebounce = 250;
+    var changeHeaderOn = 300, scrollDebounce = 350;
 
     function scrollPage() {
       $('.navbar-default').toggleClass('navbar-scroll', scrollY() >= changeHeaderOn);
